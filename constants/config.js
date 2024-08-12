db8d39861ca717226bc6185bbefa0aab92589951
harsh-sjce <harsh.sjce@gmai.com>
2024-08-13 02:52:11 +0530

    msg

---
+++ constants/config.js
@@ -0,0 +1,17 @@
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    "https://connectify-chatapp.vercel.app",
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+const CHATTU_TOKEN = "chattu-token";
+
+export { corsOptions, CHATTU_TOKEN };
+
+
+
